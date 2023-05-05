c505aeb4f38db66b14774dd27f1d82240ae48586
Victor Quiroz <vquiroz2018@alu.uct.cl>
2023-05-05 17:27:53 -0400

    Functional convination in c++ and brute force solution.

---
+++ MWC_BF/src/brute_force_algorithm.cpp
@@ -0,0 +1,51 @@
+﻿#include "brute_force_algorithm.h"
+#include <chrono>
+#define pb push_back
+
+using namespace std;
+using json = nlohmann::json;
+
+int main() {
+    // Call the Json file
+    ifstream file("votes.json");
+    json data = json::parse(file);
+
+    int n = data["votes"].size();
+    n = 30;
+    int quorum = trunc(n / 2)+1;
+    // Initialize the distance matrix
+    double** distance_matrix = (double**)malloc(n * sizeof(double*));
+    for (size_t i = 0; i < n; i++)
+    {
+        distance_matrix[i] = (double*)malloc(n * sizeof(double));
+    }
+
+    // Fill the distance matrix
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            distance_matrix[i][j] = euclidian_distance(data["votes"][i]["x"], data["votes"][i]["y"], data["votes"][j]["x"], data["votes"][j]["y"]);
+        }
+    }
+    // Time variable initialization for execution calculation
+    auto initial_time = chrono::high_resolution_clock::now();
+    cout << "Generando combinatoria!" << endl;
+    double fitness;
+    int* coalition = (int*)malloc(quorum * sizeof(int));
+    tie(fitness,coalition)=comb(n, quorum,distance_matrix);
+    cout << "Fin Combinatoria!" << endl;
+    // Stop the clock
+    auto final_time = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(final_time - initial_time).count();
+    // Convert the time taken by the algorithm to seconds
+    time_taken *= 1e-9;
+    cout << "Time:"<< fixed << time_taken << setprecision(9) << endl;
+    cout << "Minimum Fitness:" << fitness << endl;
+    cout << "Coalition:" << endl;
+    for (size_t i = 0; i < quorum; i++)
+    {
+        cout << coalition[i] << ",";
+    }
+    return 0;
+}
---
+++ MWC_BF/src/brute_force_algorithm.h
@@ -0,0 +1,75 @@
+﻿// brute_force_algorithm.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto.
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <float.h>
+#include <nlohmann/json.hpp>
+#include <tuple>
+using namespace std;
+bool vector_sort(int const& lvd, int const& rvd)
+{
+	return lvd < rvd;
+}
+
+// Function to calculate the distance between two points
+double euclidian_distance(double x1, double y1, double x2, double y2)
+{
+	double calculation = pow(pow((x2 - x1), 2) + pow((y2 - y1), 2), 1 / (double)2);
+	return calculation;
+}
+
+
+// Function to evaluate the solutions and return the fitness value
+double evaluate_solution(vector<int> pos, double** mat, int length)
+{
+	double sum = 0;
+	double val;
+	for (size_t i = 0; i <= (pos.size()-2); i++)
+	{
+		for (size_t j = i + 1; j <= (pos.size() - 1); j++)
+		{
+			val = mat[pos[i]][pos[j]];
+			sum = sum + val;
+		}
+	}
+	return sum;
+}
+tuple<double, int*> comb(int N, int K, double** distance_matrix)
+{
+	double fitness = DBL_MAX;
+	double tempFitness;
+	int count=0;
+	int* best_solution = (int*)malloc(K * sizeof(int));
+	vector <int> combination;
+	string bitmask(K, 1); // K leading 1's
+	bitmask.resize(N, 0); // N-K trailing 0's
+
+	// print integers and permute bitmask
+	do {
+		for (int i = 0; i < N; ++i) // [0..N-1] integers
+		{
+			if (bitmask[i]) combination.push_back(i);
+		}
+		tempFitness = evaluate_solution(combination, distance_matrix, N);
+		if (tempFitness < fitness)
+		{
+			fitness = tempFitness;
+			for (size_t i = 0; i < combination.size(); i++)
+			{
+				best_solution[i] = combination[i];
+			}
+		}
+		combination.clear();
+		combination.shrink_to_fit();
+		count++;
+	} while (prev_permutation(bitmask.begin(), bitmask.end()));
+	cout << "Combinaciones:" << count<<endl;
+	return { fitness,best_solution };
+}
+//Solution from https://rosettacode.org/wiki/Combinations#C.2B.2B
